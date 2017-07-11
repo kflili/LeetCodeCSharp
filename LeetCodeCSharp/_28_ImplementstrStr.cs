bba721231b860df204069a4d577321ee61d7a211
LiLi <kflili@gmail.com>
2017-07-11 15:29:23 -0400

    add 28 solution and test

---
+++ LeetCodeCSharp/_28_ImplementstrStr.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeCSharp
+{
+    public class _28_ImplementstrStr
+    {
+        /*
+        Hint: use two Pointers
+        */
+        public int StrStr(string haystack, string needle)
+        {
+            if (haystack == null || needle == null)
+            {
+                return -1;
+            }
+            int m = haystack.Length, n = needle.Length;
+            for (int i = 0; i < m - n + 1; i++)
+            {
+                int j;
+                for (j = 0; j < n; j++)
+                {
+                    if (haystack[i + j] != needle[j])
+                    {
+                        break;
+                    }
+                }
+                if (j == n)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ LeetCodeCSharpTests/_28_ImplementstrStrTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeCSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeCSharp.Tests
+{
+    [TestClass()]
+    public class _28_ImplementstrStrTests
+    {
+        [TestMethod()]
+        public void StrStrTest()
+        {
+            _28_ImplementstrStr testObj = new _28_ImplementstrStr();
+            string s = "abcddkew", t = "cdd";
+
+            Assert.AreEqual(2, testObj.StrStr(s, t));
+        }
+    }
+}
