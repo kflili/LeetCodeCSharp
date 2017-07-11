fa2a7e038cd81c701417ee641ecdaa8f9838bc44
LiLi <kflili@gmail.com>
2017-07-11 15:00:56 -0400

    add solution and test for 242

---
+++ LeetCodeCSharp/_242_ValidAnagram.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeCSharp
+{
+    /*
+    Hint: use Dictionary(HashTable)
+    loop first string, add all the char;
+    loop second string, remove all the char. Meanwhile check the dict.
+    */
+    public class _242_ValidAnagram
+    {
+        public bool IsAnagram(string s, string t)
+        {
+            if (s == null)
+            {
+                return t == null;
+            }
+            if (t == null)
+            {
+                return false;
+            }
+            if (s.Length != t.Length)
+            {
+                return false;
+            }
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!dict.ContainsKey(s[i]))
+                {
+                    dict.Add(s[i], 1);
+                }
+                else
+                {
+                    dict[s[i]] += 1;
+                }
+            }
+            for (int i = 0; i < t.Length; i++)
+            {
+                if (!dict.ContainsKey(t[i]))
+                {
+                    return false;
+                }
+                dict[t[i]]--;
+                if (dict[t[i]] < 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ LeetCodeCSharpTests/_242_ValidAnagramTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeCSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeCSharp.Tests
+{
+    [TestClass()]
+    public class _242_ValidAnagramTests
+    {
+        [TestMethod()]
+        public void IsAnagramTest()
+        {
+            _242_ValidAnagram testObj = new _242_ValidAnagram();
+            string s1 = "anagram", t1 = "nagaram";
+            string s2 = "rat", t2 = "car";
+
+            Assert.AreEqual(true, testObj.IsAnagram(s1, t1));
+            Assert.AreEqual(false, testObj.IsAnagram(s2, t2));
+        }
+    }
+}
