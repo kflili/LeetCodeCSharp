af082656e100ec4e7f0c0ac547e0d8db3cba42d1
LiLi <kflili@gmail.com>
2017-07-11 15:14:13 -0400

    add solution and test for 3

---
+++ LeetCodeCSharp/_3_LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeCSharp
+{
+    public class _3_LongestSubstringWithoutRepeatingCharacters
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            if (s == null)
+            {
+                return 0;
+            }
+            int n = s.Length, ans = 0;
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+            for (int i = 0, j = 0; j < n; j++)
+            {
+                if (!dict.ContainsKey(s[j]))
+                {
+                    dict.Add(s[j], j);
+                } else
+                {
+                    i = Math.Max(i, dict[s[j]] + 1);
+                    dict[s[j]] = j;
+                }
+                ans = Math.Max(ans, j - i + 1);
+            }
+            return ans;
+        }
+    }
+}
---
+++ LeetCodeCSharpTests/_3_LongestSubstringWithoutRepeatingCharactersTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeCSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeCSharp.Tests
+{
+    [TestClass()]
+    public class _3_LongestSubstringWithoutRepeatingCharactersTests
+    {
+        [TestMethod()]
+        public void LengthOfLongestSubstringTest()
+        {
+            _3_LongestSubstringWithoutRepeatingCharacters testObj = new _3_LongestSubstringWithoutRepeatingCharacters();
+            string[] s = { "abcabcbb", "bbbbb", "pwwkew" };
+            int[] ans = { 3, 1, 3 };
+
+            Assert.AreEqual(3, testObj.LengthOfLongestSubstring(s[0]));
+            Assert.AreEqual(1, testObj.LengthOfLongestSubstring(s[1]));
+            Assert.AreEqual(3, testObj.LengthOfLongestSubstring(s[2]));
+            
+        }
+    }
+}
