f4844741da04d21997b6fce47f436d22f5cdf359
LiLi <kflili@gmail.com>
2017-07-11 17:55:50 -0400

    add 237 solution

---
+++ LeetCodeCSharp/_237_DeleteNodeinaLinkedList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeCSharp
+{
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+    public class _237_DeleteNodeinaLinkedList
+    {
+        public void DeleteNode(ListNode node)
+        {
+            if (node != null)
+            {
+                node.val = node.next.val;
+                node.next = node.next.next;
+            }
+        }
+    }
+}
