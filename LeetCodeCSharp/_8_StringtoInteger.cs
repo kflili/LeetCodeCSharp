50bfaf42349e2008b9d0753901f6b59107a07542
LiLi <kflili@gmail.com>
2017-07-11 15:54:12 -0400

    add 8 solution and test

---
+++ LeetCodeCSharp/_8_StringtoInteger.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeCSharp
+{
+    public class _8_StringtoIntegeratoi
+    {
+        /*
+            Requirements for atoi:
+            1. trim whitespace
+            2. takes an optional '+' or '-'
+            3. when meet non-numetic char, break
+            4. If no valid conversion could be performed, 0 is returned.
+            5. If out of int range, return INT_MAX(2147483647) or INT_MIN(-2147483648)
+         */
+
+        public int MyAtoi(string str)
+        {
+            if (str == null)
+            {
+                return 0;
+            }
+            str = str.Trim();
+            if (str.Length == 0)
+            {
+                return 0;
+            }
+            int sign = 1;
+            int index = 0;
+            if (str[index] == '+')
+            {
+                index++;
+            }
+            if (str[index] == '-')
+            {
+                sign = -1;
+                index++;
+            }
+            long num = 0;
+            for (; index < str.Length; index++)
+            {
+                if (str[index] < '0' || str[index] > '9')
+                {
+                    break;
+                }
+                num = num * 10 + str[index] - '0';
+                if (num > int.MaxValue)
+                {
+                    break;
+                }
+            }
+            if (num * sign >= int.MaxValue)
+            {
+                return int.MaxValue;
+            } 
+            if (num * sign <= int.MinValue)
+            {
+                return int.MinValue;
+            }
+            return (int)num * sign;
+        }
+    }
+}
---
+++ LeetCodeCSharpTests/_8_StringtoIntegeratoiTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeCSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeCSharp.Tests
+{
+    [TestClass()]
+    public class _8_StringtoIntegeratoiTests
+    {
+        [TestMethod()]
+        public void MyAtoiTest()
+        {
+            _8_StringtoIntegeratoi testObj = new _8_StringtoIntegeratoi();
+
+            Assert.AreEqual(123, testObj.MyAtoi("123"));
+            Assert.AreEqual(0, testObj.MyAtoi("   "));
+            Assert.AreEqual(1, testObj.MyAtoi("  1aaa"));
+            Assert.AreEqual(0, testObj.MyAtoi("aaa"));
+            Assert.AreEqual(-123, testObj.MyAtoi("   -123"));
+        }
+    }
+}
