514a1241d67043c28b93a6c63792d13c966dc65f
LiLi <kflili@gmail.com>
2017-07-12 15:26:06 -0400

    add 98

---
+++ LeetCodeCSharp/_98_ValidateBinarySearchTree.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeCSharp
+{
+    public class ResultType
+    {
+        public ResultType(bool state, int max, int min)
+        {
+            IsValid = state;
+            MaxValue = max;
+            MinValue = min;
+        }
+        public bool IsValid { get; set; }
+        public int MaxValue { get; set; }
+        public int MinValue { get; set; }
+
+    }
+    public class _98_ValidateBinarySearchTree
+    {
+        public bool IsValidBST(TreeNode root)
+        {
+            var res = helper(root);
+            return res.IsValid;
+        }
+        private ResultType helper(TreeNode root)
+        {
+            if (root == null)
+            {
+                return new ResultType(true, 0, 0);
+            }
+            ResultType res = new ResultType(true, root.val, root.val);
+            if (root.left == null && root.right == null)
+            {
+                return res;
+            }
+            if (root.left != null)
+            {
+                var left = helper(root.left);
+                if (!left.IsValid || left.MaxValue >= root.val)
+                {
+                    return new ResultType(false, 0, 0);
+                }
+                res.MinValue = left.MinValue;
+            }
+            if (root.right != null)
+            {
+                var right = helper(root.right);
+                if (!right.IsValid || right.MinValue <= root.val)
+                {
+                    return new ResultType(false, 0, 0);
+                }
+                res.MaxValue = right.MaxValue;
+            }
+            return res;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -194,3 +194,29 @@ All root-to-leaf paths are:
 ["1->2->5", "1->3"]
 
 [My Solution Code](https://github.com/kflili/LeetCodeCSharp/blob/master/LeetCodeCSharp/_257_BinaryTreePaths.cs)
+
+**_98. Validate Binary Search Tree_**
+
+**Description:**
+Given a binary tree, determine if it is a valid binary search tree (BST).
+
+Assume a BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+Example 1:
+```
+    2
+   / \
+  1   3
+```
+Binary tree [2,1,3], return true.
+Example 2:
+```
+    1
+   / \
+  2   3
+```
+Binary tree [1,2,3], return false.
+
