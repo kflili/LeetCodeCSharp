7edc0dbd6c8601f91fd56426ddc3f66565856217
LiLi <kflili@gmail.com>
2017-07-12 11:02:20 -0400

    add 112

---
+++ LeetCodeCSharp/_112_PathSum.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+namespace LeetCodeCSharp
+{
+    public class _112_PathSum
+    {
+        public bool HasPathSum(TreeNode root, int sum)
+        {
+            if (root == null)
+            {
+                return false;
+            }
+            if (root.left == null && root.right == null)
+            {
+                if (sum == root.val)
+                {
+                    return true;
+                } else
+                {
+                    return false;
+                }
+            }
+            if (root.left != null && HasPathSum(root.left, sum - root.val))
+            {
+                return true;
+            } 
+            if (root.right != null && HasPathSum(root.right, sum - root.val))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -153,3 +153,23 @@ Given two binary trees, write a function to check if they are equal or not.
 Two binary trees are considered equal if they are structurally identical and the nodes have the same value.
 
 [My Solution Code](https://github.com/kflili/LeetCodeCSharp/blob/master/LeetCodeCSharp/_100_SameTree.cs)
+
+**_112. Path Sum_**
+
+**Description:**
+Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+
+For example:
+Given the below binary tree and sum = 22,
+
+              5
+             / \
+            4   8
+           /   / \
+          11  13  4
+         /  \      \
+        7    2      1
+
+return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
+
+[My Solution Code](https://github.com/kflili/LeetCodeCSharp/blob/master/LeetCodeCSharp/_112_PathSum.cs)
