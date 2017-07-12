4a9ebddf4fd6ece2cb232a783df3098d0630a6ed
LiLi <kflili@gmail.com>
2017-07-12 17:13:23 -0400

    add 141 test

---
+++ LeetCodeCSharp/_225_ImplementStackusingQueues.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeCSharp
+{
+    class _225_ImplementStackusingQueues
+    {
+    }
+    //public class MyStack
+    //{
+
+    //    /** Initialize your data structure here. */
+    //    public MyStack()
+    //    {
+
+    //    }
+
+    //    /** Push element x onto stack. */
+    //    public void Push(int x)
+    //    {
+
+    //    }
+
+    //    /** Removes the element on top of the stack and returns that element. */
+    //    public int Pop()
+    //    {
+
+    //    }
+
+    //    /** Get the top element. */
+    //    public int Top()
+    //    {
+
+    //    }
+
+    //    /** Returns whether the stack is empty. */
+    //    public bool Empty()
+    //    {
+
+    //    }
+    //}
+
+    /**
+     * Your MyStack object will be instantiated and called as such:
+     * MyStack obj = new MyStack();
+     * obj.Push(x);
+     * int param_2 = obj.Pop();
+     * int param_3 = obj.Top();
+     * bool param_4 = obj.Empty();
+     */
+}
---
+++ LeetCodeCSharpTests/_141_LinkedListCycleTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeCSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeCSharp.Tests
+{
+    [TestClass()]
+    public class _141_LinkedListCycleTests
+    {
+        [TestMethod()]
+        public void HasCycleTest()
+        {
+            _141_LinkedListCycle testObj = new _141_LinkedListCycle();
+            var list = new ListNode(1);
+            list.next = new ListNode(2);
+            list.next.next = new ListNode(3);
+            var cycleNode = new ListNode(4);
+            list.next.next.next = cycleNode;
+            list.next.next.next.next = new ListNode(5);
+            list.next.next.next.next.next = new ListNode(6);
+            list.next.next.next.next.next.next = cycleNode;
+
+            Assert.AreEqual(true, testObj.HasCycle(list));
+        }
+    }
+}
