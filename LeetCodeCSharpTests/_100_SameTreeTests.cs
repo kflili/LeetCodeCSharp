314bc438953c2c95fdfd429863f2d89317cd2c2a
LiLi <kflili@gmail.com>
2017-07-12 17:17:28 -0400

    add unit test for 100

---
+++ LeetCodeCSharpTests/_100_SameTreeTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeCSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeCSharp.Tests
+{
+    [TestClass()]
+    public class _100_SameTreeTests
+    {
+        [TestMethod()]
+        public void IsSameTreeTest()
+        {
+            TreeNode tree1 = new TreeNode(0);
+            tree1.left = new TreeNode(1);
+            tree1.right = new TreeNode(2);
+
+            TreeNode tree2 = new TreeNode(0);
+            tree2.left = new TreeNode(1);
+            tree2.right = new TreeNode(2);
+
+            TreeNode tree3 = new TreeNode(0);
+            tree3.left = new TreeNode(2);
+            tree3.right = new TreeNode(2);
+
+            _100_SameTree testObj = new _100_SameTree();
+            Assert.AreEqual(true, testObj.IsSameTree(tree1, tree2));
+            Assert.AreEqual(false, testObj.IsSameTree(tree1, tree3));
+        }
+    }
+}
