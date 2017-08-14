5a131d0b8e34106467b99bf559f4365938cc6e43
LiLi <kflili@gmail.com>
2017-08-14 13:55:16 -0400

    Revert "Revert "general template for Pre- In- and Post-Order""
    This reverts commit c6be2d6a8ea6e0304ee99bfa415e3589607a44f8.

--- LeetCodeCSharp/TreeNode.cs
+++ LeetCodeCSharp/TreeNode.cs
@@ -12,5 +12,27 @@ namespace LeetCodeCSharp
         public TreeNode left;
         public TreeNode right;
         public TreeNode(int x) { val = x; }
+
+        public static TreeNode BuildTree()
+        {
+            /*
+             *            1
+             *          /   \
+             *         2     3
+             *        / \   / \
+             *       4   5 6   7
+             * 
+             **/
+            var root = new TreeNode(1)
+            {
+                left = new TreeNode(2),
+                right = new TreeNode(3)
+            };
+            root.left.left = new TreeNode(4);
+            root.left.right = new TreeNode(5);
+            root.right.left = new TreeNode(6);
+            root.right.right = new TreeNode(7);
+            return root;
+        }
     }
 }
